4afdac58fd6e43dba2182a56a2081f1a9b7e8517
Zach <104291766+zachary-o@users.noreply.github.com>
2025-04-26 23:17:07 +0700

    add responsive, fetch data from server

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,83 +1,89 @@
-"use client"
+"use client";
 
-import { mockData } from "@/mockData"
-import type { PaginationProps } from "antd"
-import { Button, TimePicker } from "antd"
+import { LoadingOutlined } from "@ant-design/icons";
+import type { PaginationProps } from "antd";
+import { Button, DatePicker, Flex, Pagination, Spin, TimePicker } from "antd";
+import { useEffect, useState } from "react";
+import Flight from "./Flight";
 
-import { DatePicker, Pagination } from "antd"
-import { useEffect, useState } from "react"
-import Flight from "./Flight"
-
-import dayjs, { Dayjs } from "dayjs"
-import utc from "dayjs/plugin/utc"
+import dayjs, { Dayjs } from "dayjs";
+import utc from "dayjs/plugin/utc";
 import {
   FlightApiResponse,
+  FlightData,
   MappedFlightData,
   Pagination as PaginationType,
-} from "./types"
-dayjs.extend(utc)
+} from "./types";
+dayjs.extend(utc);
 
 const App = () => {
-  const format = "HH:mm"
+  const format = "HH:mm";
 
-  const [flights, setFlights] = useState<FlightApiResponse | null>(null)
-  const [currentPage, setCurrentPage] = useState<number>(1)
-  const [pageSize, setPageSize] = useState<number>(10)
+  const [isLoading, setIsLoading] = useState(false);
+  const [originalFlights, setOriginalFlights] = useState<MappedFlightData[]>(
+    []
+  );
+  const [flights, setFlights] = useState<FlightApiResponse | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
   const [paginatedFlights, setPaginatedFlights] = useState<MappedFlightData[]>(
     []
-  )
+  );
   const [time, setTime] = useState({
     timeFrom: "",
     dateFrom: "",
     timeTo: "",
     dateTo: "",
-  })
+  });
 
-  const onChangeDates = (
-    date: [Dayjs | null, Dayjs | null] | null,
-    dateString: [string, string]
-  ) => {
-    const utcStart = date?.[0]?.utc().format()
-    const utcEnd = date?.[1]?.utc().format()
+  const onChangeDates = (date: [Dayjs | null, Dayjs | null] | null) => {
+    const utcStart = date?.[0]?.utc().format();
+    const utcEnd = date?.[1]?.utc().format();
 
     setTime((prev) => {
       return {
         ...prev,
         dateFrom: utcStart || "",
         dateTo: utcEnd || "",
-      }
-    })
-  }
+      };
+    });
+  };
 
   const onChangeTime = (
     time: [Dayjs | null, Dayjs | null] | null,
     timeString: [string, string]
   ) => {
-    const timeFrom = timeString[0]
-    const timeTo = timeString[1]
+    const timeFrom = timeString[0];
+    const timeTo = timeString[1];
 
     setTime((prev) => {
       return {
         ...prev,
         timeFrom: timeFrom || "",
         timeTo: timeTo || "",
-      }
-    })
-  }
+      };
+    });
+  };
 
-  const onPaginationChange: PaginationProps["onChange"] = (page, pageSize) => {
-    setCurrentPage(page)
-    setPageSize(pageSize)
-  }
-
-  const fetchData = async () => {
+  const fetchData = async (params: { offset?: number } = {}) => {
+    setIsLoading(true);
     try {
-      //   const response = await fetch(
-      //     `${process.env.NEXT_PUBLIC_API_URL}access_key=${process.env.NEXT_PUBLIC_SECRET_API_KEY}`
-      //   );
-      //   const data = await response.json();
-      const mappedFlights: MappedFlightData[] = mockData.data.map(
-        (flight: any) => {
+      const { offset = 0 } = params;
+      const queryParams = new URLSearchParams({
+        access_key: process.env.NEXT_PUBLIC_SECRET_API_KEY || "",
+        limit: "100",
+        offset: offset.toString(),
+      });
+
+      const url = `${
+        process.env.NEXT_PUBLIC_API_URL
+      }?${queryParams.toString()}`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      const mappedFlights: MappedFlightData[] = data.data.map(
+        (flight: FlightData) => {
           return {
             flightStatus: flight.flight_status,
             departureTime: flight.departure.scheduled,
@@ -92,111 +98,169 @@ const App = () => {
             arrivalGate: flight.arrival.gate,
             airline: flight.airline.name,
             flightNumber: flight.flight.number,
-          }
+          };
         }
-      )
-
+      );
       const pagination: PaginationType = {
-        limit: 100,
-        offset: 0,
-        count: mappedFlights.length,
-        total: mappedFlights.length,
-      }
+        limit: data.pagination.limit,
+        offset: data.pagination.offset,
+        count: data.pagination.count,
+        total: data.pagination.total,
+      };
       setFlights({
         data: mappedFlights,
         pagination,
-      })
+      });
+      setOriginalFlights(mappedFlights);
     } catch (error) {
-      console.log("error", error)
+      console.log("error", error);
+    } finally {
+      setIsLoading(false);
     }
-  }
+  };
+
   useEffect(() => {
-    fetchData()
-  }, [])
-  // console.log("flights", flights)
-  // console.log('time', time)
+    fetchData();
+  }, []);
 
   useEffect(() => {
     if (flights) {
-      const startIndex = (currentPage - 1) * pageSize
-      const endIndex = startIndex + pageSize
-      const sliced = flights.data.slice(startIndex, endIndex)
-      setPaginatedFlights(sliced as MappedFlightData[])
+      const startIndex = (currentPage - 1) * pageSize;
+      const endIndex = startIndex + pageSize;
+      const sliced = flights.data.slice(startIndex, endIndex);
+      setPaginatedFlights(sliced as MappedFlightData[]);
     }
-  }, [flights, currentPage, pageSize])
-  // {timeFrom: '01:00', dateFrom: '2025-04-19T17:00:00Z', timeTo: '07:30', dateTo: '2025-04-25T17:00:00Z'}
+  }, [flights, currentPage, pageSize]);
+
+  const onPaginationChange: PaginationProps["onChange"] = (page, pageSize) => {
+    setCurrentPage(page);
+    setPageSize(pageSize);
+
+    const offset = (page - 1) * pageSize;
+    fetchData({ offset });
+  };
+
+  const handleRestFilters = () => {
+    setTime({
+      timeFrom: "",
+      dateFrom: "",
+      timeTo: "",
+      dateTo: "",
+    });
+    setFlights((prev) => ({
+      ...prev!,
+      data: originalFlights,
+    }));
+    setPaginatedFlights(originalFlights.slice(0, pageSize));
+    setCurrentPage(1);
+  };
 
   const handleFilter = () => {
-    if (!flights) return
-  
-    const { dateFrom, dateTo, timeFrom, timeTo } = time
-  
+    if (!flights) return;
+
+    const { dateFrom, dateTo, timeFrom, timeTo } = time;
+
     const filtered = flights.data.filter((flight) => {
-      const departure = dayjs.utc(flight.departureDate)
-  
+      const departure = dayjs.utc(flight.departureDate);
+
       const fromDateTime =
         dateFrom && timeFrom
           ? dayjs.utc(`${dayjs(dateFrom).format("YYYY-MM-DD")}T${timeFrom}`)
           : dateFrom
           ? dayjs.utc(dateFrom)
-          : null
-  
+          : null;
+
       const toDateTime =
         dateTo && timeTo
           ? dayjs.utc(`${dayjs(dateTo).format("YYYY-MM-DD")}T${timeTo}`)
           : dateTo
           ? dayjs.utc(dateTo)
-          : null
-  
+          : null;
+
       const isAfterStart = fromDateTime
         ? departure.isAfter(fromDateTime) || departure.isSame(fromDateTime)
-        : true
+        : true;
       const isBeforeEnd = toDateTime
         ? departure.isBefore(toDateTime) || departure.isSame(toDateTime)
-        : true
-  
-      return isAfterStart && isBeforeEnd
-    })
-  
-    const newFlights = { ...flights, data: filtered }
-    console.log("newFlights", newFlights)
-    setFlights(newFlights)
-    setCurrentPage(1)
-    setPaginatedFlights(newFlights.data.slice(0, pageSize))
-  }
-  
+        : true;
+
+      return isAfterStart && isBeforeEnd;
+    });
+
+    const newFlights = { ...flights, data: filtered };
+    setFlights(newFlights);
+    setCurrentPage(1);
+    setPaginatedFlights(newFlights.data.slice(0, pageSize));
+  };
 
   return (
     <>
-      <header className="w-full h-20 p-2 border-b border-b-violet-500">
-        <div className="flex justify-between mx-auto max-w-screen-md">
-          <DatePicker.RangePicker onChange={onChangeDates} />
+      <header className="w-full mx-auto h-20 p-2 flex items-center justify-center border-b border-b-violet-500 max-w-screen-2xl">
+        <div className="w-full flex flex-row gap-4 ">
+          <DatePicker.RangePicker
+            value={[
+              time.dateFrom ? dayjs(time.dateFrom) : null,
+              time.dateTo ? dayjs(time.dateTo) : null,
+            ]}
+            onChange={onChangeDates}
+          />
           <TimePicker.RangePicker
-            format={format}
-            onChange={onChangeTime}
             minuteStep={5}
+            format={format}
+            value={[
+              time.timeFrom ? dayjs(time.timeFrom, format) : null,
+              time.timeTo ? dayjs(time.timeTo, format) : null,
+            ]}
+            onChange={onChangeTime}
           />
           <Button color="primary" variant="filled" onClick={handleFilter}>
             Search
           </Button>
+          {(time.dateFrom !== "" ||
+            time.dateTo !== "" ||
+            time.timeFrom !== "" ||
+            time.timeTo !== "") && (
+            <Button color="danger" variant="text" onClick={handleRestFilters}>
+              Reset
+            </Button>
+          )}
         </div>
       </header>
-      <div className="w-full p-2 flex flex-col gap-4 justify-start max-w-screen-md">
-        {flights &&
-          flights.data.length > 0 &&
-          paginatedFlights.map((flight: MappedFlightData) => (
-            <Flight key={`${flight.flightNumber}`} {...flight} />
-          ))}
-        <Pagination
-          current={currentPage}
-          pageSize={pageSize}
-          total={flights?.data.length || 0}
-          onChange={onPaginationChange}
-          showSizeChanger
-        />
+      <div className="w-full mx-auto p-2 flex flex-col gap-4 max-w-screen-2xl">
+        {!isLoading && (
+          <>
+            {flights && paginatedFlights.length > 0 ? (
+              <>
+                {paginatedFlights.map((flight: MappedFlightData) => (
+                  <Flight key={flight.flightNumber} {...flight} />
+                ))}
+                <Pagination
+                  className="self-end"
+                  current={currentPage}
+                  pageSize={pageSize}
+                  total={flights?.data.length || 0}
+                  onChange={onPaginationChange}
+                  showSizeChanger
+                />
+              </>
+            ) : (
+              <div className="text-center text-gray-500 text-xl mt-10">
+                No flights found.
+              </div>
+            )}
+          </>
+        )}
       </div>
-    </>
-  )
-}
 
-export default App
+      <Flex align="center" gap="middle">
+        <Spin
+          indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />}
+          spinning={isLoading}
+          fullscreen
+        />
+      </Flex>
+    </>
+  );
+};
+
+export default App;
--- src/app/components/Flight.tsx
+++ src/app/components/Flight.tsx
@@ -1,13 +1,13 @@
-import dayjs from "dayjs"
-import utc from "dayjs/plugin/utc"
-import Image from "next/image"
-import { MappedFlightData } from "./types"
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import Image from "next/image";
+import { MappedFlightData } from "./types";
 
-dayjs.extend(utc)
+dayjs.extend(utc);
 
 const Flight = (props: MappedFlightData) => {
   const {
-    flightStatus,
+    // flightStatus,
     departureTime,
     departureDate,
     departureAirport,
@@ -18,30 +18,30 @@ const Flight = (props: MappedFlightData) => {
     arrivalAirport,
     arrivalTerminal,
     arrivalGate,
-    airline,
-    flightNumber,
-  } = props
+    // airline,
+    // flightNumber,
+  } = props;
 
-  const departure = dayjs.utc(departureTime)
-  const arrival = dayjs.utc(arrivalTime)
+  const departure = dayjs.utc(departureTime);
+  const arrival = dayjs.utc(arrivalTime);
 
-  const durationInMinutes = arrival.diff(departure, "minute")
-  const hours = Math.floor(durationInMinutes / 60)
-  const minutes = durationInMinutes % 60
+  const durationInMinutes = arrival.diff(departure, "minute");
+  const hours = Math.floor(durationInMinutes / 60);
+  const minutes = durationInMinutes % 60;
   return (
-    <div className="w-full p-2 border border-gray-500 rounded-sm flex flex-row items-center gap-2">
+    <div className="w-full flex-1 p-2 border border-gray-500 rounded-sm flex flex-row items-center gap-2">
       {/* DEPARTURE */}
       <div className="flex-1 min-w-0 flex flex-col items-center justify-center">
-        <p className="text-ellipsis whitespace-nowrap w-full text-center font-semibold">
+        <p className="text-ellipsis lg:whitespace-nowrap w-full text-center font-semibold text-wrap text-sm lg:text-lg">
           {dayjs(departureDate).format("MMMM D, YYYY")} - Departure
         </p>
-        <h2 className="text-ellipsis whitespace-nowrap w-full text-center text-4xl font-extrabold">
+        <h2 className="text-ellipsis lg:whitespace-nowrap w-full text-center text-4xl font-extrabold">
           {departure.format("HH:mm")}
         </h2>
-        <p className="text-ellipsis whitespace-nowrap w-full text-center text-gray-500">
+        <p className="text-ellipsis lg:whitespace-nowrap w-full text-center text-gray-500 text-wrap text-sm">
           {departureAirport}
         </p>
-        <p className="text-ellipsis whitespace-nowrap w-full text-center text-gray-500">
+        <p className="text-ellipsis lg:whitespace-nowrap w-full text-center text-gray-500 text-sm">
           Terminal: {departureTerminal ? "--" : departureTerminal}, Gate:{" "}
           {departureGate === null ? "--" : departureGate}
         </p>
@@ -51,7 +51,7 @@ const Flight = (props: MappedFlightData) => {
 
       <div className="relative">
         <Image src="/airplane.png" alt="airplane" width={24} height={24} />
-        <p className="absolute top-6 right-2 text-ellipsis whitespace-nowrap w-full text-center">
+        <p className="absolute top-6 right-2 text-nowrap lg:text-ellipsis lg:whitespace-nowrap w-full text-center">
           {hours}h {minutes}m
         </p>
       </div>
@@ -60,21 +60,21 @@ const Flight = (props: MappedFlightData) => {
 
       {/* ARRIVAL */}
       <div className="flex-1 min-w-0 flex flex-col items-center justify-center">
-        <p className="text-ellipsis whitespace-nowrap w-full text-center font-semibold">
+        <p className="text-ellipsis lg:whitespace-nowrap w-full text-center font-semibold text-wrap text-sm">
           {dayjs(arrivalDate).format("MMMM D, YYYY")} - Arrival
         </p>
-        <h2 className="text-ellipsis whitespace-nowrap w-full text-center text-4xl font-extrabold">
+        <h2 className="text-ellipsis lg:whitespace-nowrap w-full text-center text-4xl font-extrabold">
           {arrival.format("HH:mm")}
         </h2>
-        <p className="text-ellipsis whitespace-nowrap w-full text-center text-gray-500">
+        <p className="text-ellipsis lg:whitespace-nowrap w-full text-center text-gray-500 text-wrap text-sm">
           {arrivalAirport}
         </p>
-        <p className="text-ellipsis whitespace-nowrap w-full text-center text-gray-500">
+        <p className="text-ellipsis lg:whitespace-nowrap w-full text-center text-gray-500 text-sm text-md">
           Terminal: {arrivalTerminal === null ? "--" : arrivalTerminal}, Gate:{" "}
           {arrivalGate === null ? "--" : arrivalGate}
         </p>
       </div>
     </div>
-  )
-}
-export default Flight
+  );
+};
+export default Flight;
